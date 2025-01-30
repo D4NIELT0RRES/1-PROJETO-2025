3eb1843a61d4b2c826378f5aa078b99de1e2dc0d
DANIEL GOMES TORRES <24122277@CFP127ALUNO.LOCAL>
2025-01-30 15:33:30 -0300

    Base doprojeto

---
+++ modulo/funcoes.js
@@ -0,0 +1,14 @@
+/****************************************************************************************************
+ * Objetivo: Desenvolver um pequeno software com estados,região, etc...
+ * Data:30/01/2024
+ * Autor: Daniel
+ * Versão: 1.0
+ ****************************************************************************************************/
+
+/*
+    Para criar uma API devemos instalar:
+
+    express        npm install express --save         --> Serve para criar uma API
+    cors           npm install cors --save            --> Serve para configurar as permissões da API
+    body-parser    npm install body-parser --save     --> Serve para manipular os dados do body da API
+*/
